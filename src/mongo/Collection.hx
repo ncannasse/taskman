0e1ce2e7f683deb41a076cc814375b662f218b12
ncannasse <ncannasse@gmail.com>
2017-11-21 14:41:39 +0100

    minor

--- src/mongo/Collection.hx
+++ src/mongo/Collection.hx
@@ -2,7 +2,7 @@ package mongo;
 
 extern class Collection {
 
-    function aggregate( pipeline : Dynamic, ?options : Dynamic, ?callback : MongoError -> Dynamic -> Void ) : AggregationCursor;
+    function aggregate( pipeline : Dynamic, ?options : {}, ?callback : MongoError -> Dynamic -> Void ) : AggregationCursor;
     function insertMany( objs : Array<Dynamic>, callback : MongoError -> Dynamic -> Void ) : Void;
     function deleteMany( query : Dynamic, callback : MongoError -> Dynamic -> Void ) : Void;
     function find( ?query : Dynamic, ?callback : MongoError -> Dynamic -> Void ) : Cursor;
